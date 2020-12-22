e597cbd8bcf118ae73341c006671188299044e03
Amon Shokhin Ahmed <amon.shokhin.ahmed@gmail.com>
2020-12-22 15:55:00 +0100

    Clean up KDTree code

--- KDTree/KDTree/KDTree.cpp
+++ KDTree/KDTree/KDTree.cpp
@@ -1,44 +1,11 @@
 #include "KDTree.h"
-#include <sstream>
-#include <iomanip>
-
-int KDTree::compareVertices(glm::vec3 a, glm::vec3 b, unsigned int j)
-{
-    switch (j) {
-    case 0:
-        return a.x == b.x ? 0 : (a.x < b.x ? -1 : 1);
-        break;
-    case 1:
-        return a.y == b.y ? 0 : (a.y < b.y ? -1 : 1);
-        break;
-    default:
-        return a.z == b.z ? 0 : (a.z < b.z ? -1 : 1);
-        break;
-    }
-}
-static bool xCompare(glm::vec3 a, glm::vec3 b)
-{
-    return (a.x < b.x);
-}
-static bool yCompare(glm::vec3 a, glm::vec3 b)
-{
-    return (a.y < b.y);
-}
-static bool zCompare(glm::vec3 a, glm::vec3 b)
-{
-    return (a.z < b.z);
-}
-static string vec3ToString(glm::vec3 v)
-{
-    std::stringstream stream;
-    stream << std::fixed << std::setprecision(1) << v.x << "|" << v.y << "|" << v.z;
-    return stream.str();
-}
-
 
 KDTree::KDTree(std::vector<glm::vec3> vertices, bool visual)
 {
-	buildTree(vertices);
+    if (vertices.size() <= 0) {
+        return;
+    }
+    insertVertices(vertices, 0, vertices.size());
 	if (visual) {
 		generateLines();
 	}
@@ -48,7 +15,6 @@ glm::vec3 KDTree::FindMin(unsigned int j)
 {
     return findMinFromNode(root, j);
 }
-
 glm::vec3 KDTree::FindMax(unsigned int j)
 {
     return findMaxFromNode(root,j);
@@ -62,29 +28,53 @@ KDTree::~KDTree()
 	_lines.clear();
 }
 
-
-void KDTree::buildTree(std::vector<glm::vec3>vertices)
+glm::vec3 KDTree::findMinFromNode(Node* node, unsigned int j)
 {
-    if (vertices.size() <= 0) {
-        return;
+    if (node == NULL) {
+        return glm::vec3(INT_MAX);
     }
-    min = glm::vec3(0,0,0);
-    max = glm::vec3(0,0,0);
 
-    min.x = std::min_element(vertices.begin(), vertices.end(), xCompare)->x;
-    min.y = std::min_element(vertices.begin(), vertices.end(), yCompare)->y;
-    min.z = std::min_element(vertices.begin(), vertices.end(), zCompare)->z;
+    if (node->j == j) {
+        if (node->left == nullptr) {
+            return node->vertex;
+        }
+        else {
+            glm::vec3 a = node->vertex;
+            glm::vec3 b = findMinFromNode(node->left, j);
+            return Vec3Comparator(j)(a, b) ? a : b;
+        }
+    }
 
-    max.x = std::max_element(vertices.begin(), vertices.end(), xCompare)->x;
-    max.y = std::max_element(vertices.begin(), vertices.end(), yCompare)->y;
-    max.z = std::max_element(vertices.begin(), vertices.end(), zCompare)->z;
+    glm::vec3 a = node->vertex;
+    glm::vec3 b = findMinFromNode(node->left, j);
+    glm::vec3 c = findMinFromNode(node->right, j);
+    return Vec3Comparator(j)(a, b) ? (Vec3Comparator(j)(a, c) ? a : c) : (Vec3Comparator(j)(b, c) ? b : c);
+}
+glm::vec3 KDTree::findMaxFromNode(Node* node, unsigned int j)
+{
+    if (node == NULL) {
+        return glm::vec3(INT_MIN);
+    }
 
-    
-    insertVertices(vertices, 0, vertices.size());
+    if (node->j == j) {
+        if (node->right == nullptr) {
+            return node->vertex;
+        }
+        else {
+            glm::vec3 a = node->vertex;
+            glm::vec3 b = findMaxFromNode(node->right, j);
+            return Vec3Comparator(j)(a, b) ? b : a;
+        }
+    }
+
+    glm::vec3 a = node->vertex;
+    glm::vec3 b = findMaxFromNode(node->left, j);
+    glm::vec3 c = findMaxFromNode(node->right, j);
+    return  Vec3Comparator(j)(a, b) ? (Vec3Comparator(j)(b, c) ? c : b) : (Vec3Comparator(j)(a, c) ? c : a);
 }
 
-
-void KDTree::insertVertices(std::vector<glm::vec3> vertices, unsigned int start, unsigned int end) {
+#pragma region Tree Building
+void KDTree::insertVertices(std::vector<glm::vec3> &vertices, unsigned int start, unsigned int end) {
     if (start >= end) {
         return;
     }
@@ -93,13 +83,13 @@ void KDTree::insertVertices(std::vector<glm::vec3> vertices, unsigned int start,
     glm::vec3 currentMin;
 	glm::vec3 currentMax;
 
-	currentMin.x = std::min_element(vertices.begin() + start, vertices.begin() + end, xCompare)->x;
-	currentMin.y = std::min_element(vertices.begin() + start, vertices.begin() + end, yCompare)->y;
-	currentMin.z = std::min_element(vertices.begin() + start, vertices.begin() + end, zCompare)->z;
+	currentMin.x = std::min_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(0))->x;
+	currentMin.y = std::min_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(1))->y;
+	currentMin.z = std::min_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(2))->z;
 
-	currentMax.x = std::max_element(vertices.begin() + start, vertices.begin() + end, xCompare)->x;
-	currentMax.y = std::max_element(vertices.begin() + start, vertices.begin() + end, yCompare)->y;
-	currentMax.z = std::max_element(vertices.begin() + start, vertices.begin() + end, zCompare)->z;
+	currentMax.x = std::max_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(0))->x;
+	currentMax.y = std::max_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(1))->y;
+	currentMax.z = std::max_element(vertices.begin() + start, vertices.begin() + end, Vec3Comparator(2))->z;
 
     //for (int i = start; i < end; i++) {
     //    std::cout << vec3ToString(vertices.at(i)) << std::endl;
@@ -110,7 +100,7 @@ void KDTree::insertVertices(std::vector<glm::vec3> vertices, unsigned int start,
 	int j = span.x >= span.y ? (span.x >= span.z ? 0 : 2) : (span.y >= span.z ? 1 : 2);
 	//std::cout << " therefore j is " << j << std::endl;
 	int i = start + ((end - start) / 2);
-	std::nth_element(vertices.begin() + start, vertices.begin() + i, vertices.begin() + end, j == 0 ? xCompare : (j == 1 ? yCompare : zCompare));
+	std::nth_element(vertices.begin() + start, vertices.begin() + i, vertices.begin() + end, Vec3Comparator(j));
 
 
 	//std::cout << std::endl << "after nth element: " << std::endl;
@@ -125,14 +115,51 @@ void KDTree::insertVertices(std::vector<glm::vec3> vertices, unsigned int start,
 	insertVertices(vertices, i + 1, end);
 	return;
 }
+void KDTree::insertNode(Node*& node, glm::vec3 vertex, unsigned int j)
+{
+    if (node == nullptr) {
+        node = new Node();
+        node->vertex = vertex;
+        node->j = j;
+        node->left = nullptr;
+        node->right = nullptr;
+        //std::cout << " -> " << node << "." << std::endl;
+        return;
+    }
 
+    //left is smaller
+    if (Vec3Comparator(node->j)(vertex, node->vertex)) {
+        //std::cout << " -> left ";
+        insertNode(node->left, vertex, j);
+    }
+    else {
+        //std::cout << " -> right ";
+        insertNode(node->right, vertex, j);
+    }
+    return;
+}
+#pragma endregion 
+
+#pragma region Visualisation
 void KDTree::generateLines()
 {
     //std::cout << "Generating Lines" << std::endl;
     //drawBoundingBoxForNode(root, 0, 3);
+
+	glm::vec3 min = glm::vec3
+    (
+        FindMin(0).x,
+        FindMin(1).y,
+        FindMin(2).z
+    );
+	glm::vec3 max = glm::vec3
+    (
+        FindMax(0).x,
+        FindMax(1).y,
+        FindMax(2).z
+    );
     drawGridPlanesForNode(root,min,max ,0, 3);
 }
-
 void KDTree::drawLinesForAABB(glm::vec3 min, glm::vec3 max, glm::vec4 color)
 {
     glm::vec3 a = min;
@@ -220,7 +247,6 @@ dh->GetComponent<Line>()->SetPoints(d, h);
 dh->GetComponent<Line>()->Color = color;
 _lines.push_back(dh);
 }
-
 void KDTree::drawGridPlanesForNode(Node* node, glm::vec3 min, glm::vec3 max, unsigned int depth, unsigned int maxDepth)
 {
     //std::cout << "Depth " << depth<<std::endl;
@@ -257,8 +283,6 @@ void KDTree::drawGridPlanesForNode(Node* node, glm::vec3 min, glm::vec3 max, uns
 
     return;
 }
-
-
 void KDTree::drawBoundingBoxForNode(Node* node, unsigned int depth, unsigned int maxDepth)
 {
     //std::cout << "Depth " << depth<<std::endl;
@@ -276,115 +300,7 @@ void KDTree::drawBoundingBoxForNode(Node* node, unsigned int depth, unsigned int
 
     return;
 }
+#pragma endregion
 
-glm::vec3 KDTree::findMinFromNode(Node* node, unsigned int j)
-{
-    if (node == NULL) {
-        return glm::vec3(INT_MAX);
-    }
 
-    if (node->j == j) {
-        if (node->left == nullptr){
-            return node->vertex;
-        }
-        else {
-            glm::vec3 a = node->vertex;
-            glm::vec3 b = findMinFromNode(node->left, j);
-            switch (j) {
-            case 0:
-                return xCompare(a, b) ? a : b;
-                break;
-            case 1:
-                return yCompare(a, b) ? a : b;
-                break;
-            default:
-                return zCompare(a, b) ? a : b;
-                break;
-            }
-        }
-    }
-
-    glm::vec3 a = node->vertex;
-    glm::vec3 b = findMinFromNode(node->left, j);
-    glm::vec3 c = findMinFromNode(node->right, j);
-    switch (j) {
-    case 0:
-        return xCompare(a, b) ? (xCompare(a,c)?a:c) : (xCompare(b,c)?b:c);
-        break;
-    case 1:
-        return yCompare(a, b) ? (yCompare(a, c) ? a : c) : (yCompare(b, c) ? b : c);
-        break;
-    default:
-        return zCompare(a, b) ? (zCompare(a, c) ? a : c) : (zCompare(b, c) ? b : c);
-        break;
-    }
-}
-
-glm::vec3 KDTree::findMaxFromNode(Node* node, unsigned int j)
-{
-    if (node == NULL) {
-        return glm::vec3(INT_MIN);
-    }
-
-    if (node->j == j) {
-        if (node->right == nullptr) {
-            return node->vertex;
-        }
-        else {
-            glm::vec3 a = node->vertex;
-            glm::vec3 b = findMaxFromNode(node->right, j);
-            switch (j) {
-            case 0:
-                return xCompare(a, b) ? b : a;
-                break;
-            case 1:
-                return yCompare(a, b) ? b : a;
-                break;
-            default:
-                return zCompare(a, b) ? b : a;
-                break;
-            }
-        }
-    }
-
-    glm::vec3 a = node->vertex;
-    glm::vec3 b = findMaxFromNode(node->left, j);
-    glm::vec3 c = findMaxFromNode(node->right, j);
-    switch (j) {
-    case 0:
-        return xCompare(a, b) ? (xCompare(b, c) ? c : b) : (xCompare(a, c) ? c : a);
-        break;
-    case 1:
-        return yCompare(a, b) ? (yCompare(b, c) ? c : b) : (yCompare(a, c) ? c : a);
-        break;
-    default:
-        return zCompare(a, b) ? (zCompare(b, c) ? c : b) : (zCompare(a, c) ? c : a);
-        break;
-    }
-}
-
-Node* KDTree::insertNode(Node*& node, glm::vec3 vertex, unsigned int j)
-{
-    if (node == nullptr) {
-        node = new Node();
-        node->vertex = vertex;
-        node->j = j;
-        node->left = nullptr;
-        node->right = nullptr;
-        //std::cout << " -> " << node << "." << std::endl;
-        return node;
-    }
-
-    //left is smaller
-    if (compareVertices(vertex, node->vertex, node->j) < 0) {
-        //std::cout << " -> left ";
-        insertNode(node->left, vertex, j);
-	}
-	else {
-		//std::cout << " -> right ";
-        insertNode(node->right, vertex, j);
-	}
-
-	return node;
-}
 
--- KDTree/KDTree/KDTree.h
+++ KDTree/KDTree/KDTree.h
@@ -1,13 +1,40 @@
 #pragma once
 #include <vector>
 #include "Entity.h";
+#include <sstream>
+#include <iomanip>
 
 struct Node {
 	glm::vec3 vertex;
 	unsigned int j;
 	Node* left;
 	Node* right;
+}; 
+struct Vec3Comparator {
+	Vec3Comparator(unsigned int j) :J(j) { }
+	bool operator () (glm::vec3 a, glm::vec3 b) {
+		switch (J) {
+		case 0:
+			return (a.x < b.x);
+			break;
+		case 1:
+			return (a.y < b.y);
+			break;
+		default:
+			return (a.z < b.z);
+			break;
+		}
+	}
+
+	int J;
 };
+static string vec3ToString(glm::vec3 v)
+{
+	std::stringstream stream;
+	stream << std::fixed << std::setprecision(1) << v.x << "|" << v.y << "|" << v.z;
+	return stream.str();
+}
+
 class KDTree
 {
 public:
@@ -16,19 +43,19 @@ public:
 	glm::vec3 FindMax(unsigned int j);
 	~KDTree();
 private:
-	Node* insertNode(Node*& root, glm::vec3 vertex, unsigned int j);
-	void insertVertices( std::vector<glm::vec3> vertices, unsigned int start, unsigned int end);
-	int compareVertices(glm::vec3 a, glm::vec3 b, unsigned int j);
-	void buildTree(std::vector<glm::vec3 >vertices);
+	Node* root;
+	std::vector<Entity*> _lines;
+
+	glm::vec3 findMinFromNode(Node* node, unsigned int j);
+	glm::vec3 findMaxFromNode(Node* node, unsigned int j);
+
+	void insertVertices(std::vector<glm::vec3>& vertices, unsigned int start, unsigned int end);
+	void insertNode(Node*& root, glm::vec3 vertex, unsigned int j);
+
 	void generateLines();
 	void drawLinesForAABB(glm::vec3 min, glm::vec3 max, glm::vec4 color);
 	void drawGridPlanesForNode(Node* node, glm::vec3 min, glm::vec3 max, unsigned int depth, unsigned int maxDepth);
-	void drawBoundingBoxForNode(Node* node,unsigned int depth, unsigned int maxDepth);
-	std::vector<Entity*> _lines;
-	Node* root;
-	glm::vec3 min;
-	glm::vec3 max;
-	glm::vec3 findMinFromNode(Node* node, unsigned int j);
-	glm::vec3 findMaxFromNode(Node* node, unsigned int j);
+	void drawBoundingBoxForNode(Node* node, unsigned int depth, unsigned int maxDepth);
+	
 };
 
